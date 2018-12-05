27421fd49227002ae97bffc53f4c74dc04fc512f
Marco <marco.schlinger@googlemail.com>
2018-12-05 12:54:23 +0100

    Grundgerüst der Main und Makefile erweitert

--- RungeKutta4_GaggianoDoanSchlinger/main.c
+++ RungeKutta4_GaggianoDoanSchlinger/main.c
@@ -4,88 +4,35 @@
 
 int main (){
 
+	//Erstelle Pointer auf Matrizen A1, A2, A3
 
-	//Beispiel-Test mit der matrixmath Bibliothek:
-
-
-	//Erstelle Matrizen A,B und Vektor v:
-
-	struct matrix A,B;
-	struct vector v, v_A, v_B;
+	struct matrix *pmA1;
+	struct matrix *pmA2;
+	struct matrix *pmA3;
 
 
-	struct matrix *pmA;
-	struct matrix *pmB;
-	struct vector *pvV;
-    struct vector *pvV_A;
-	struct vector *pvV_B;
-
-	//Zeiger initialisieren:
-
-	pmA = &A;
-	pmB = &B;
-
-	pvV = &v;
-	pvV_A = &v_A;
-	pvV_B = &v_B;
-
-	//Initialisiere Matrizen und Vektoren:
-	init_mat(pmA,2,2);
-	init_mat(pmB,2,2);
-
-	init_vec(pvV,2);
-	init_vec(pvV_A,2);
-	init_vec(pvV_B,2);
-
-	//Setze Einträge der Matrizen und Vektoren:
-
-	//Matrix A
-
-	zero_matrix(pmA); // Matrix A = Nullmatrix
-
-	//Matrix B
-	matrix_setval(pmB,0,0,1); //Matrix B = Einheitsmatrix
-	matrix_setval(pmB,0,1,0);
-	matrix_setval(pmB,1,0,0);
-	matrix_setval(pmB,1,1,1);
-
-	//Vektor v
-	vector_setval(pvV,0,1);
-	vector_setval(pvV,1,1);
-
-	//Printe A,B und v auf die Console:
-
-	 /* mprint(pmA);
-		printf("\n");
-		mprint(pmB);
-		printf("\n");
-		vprint(pvV); */
-
-	//MatrixVektor-Multiplikationen:
-
-	mv_mult(pmA,pvV,pvV_A); // A * v = v_A
-	mv_mult(pmB,pvV,pvV_B); // B * v = v_B
+	//Erstelle Pointer auf Initial Condition Vektoren
+	struct vector *pvIC1;
+	struct vector *pvIC2;
+	struct vector *pvIC3;
 
 
-	//Printe die neuen Vektoren v_A und v_B aus:
+	//Funktionsaufrufe um Arrays und ICs zu erstellen und sie den bisher ziellosen Pointern zuzuweisen
+	buildMatrixA1(pmA1, pvIC1);
+	buildMatrixA2(pmA2, pvIC2);
+	buildMatrixA3(pmA3, pvIC3);
 
-	printf("Vektor V_A:\n");
-	vprint(pvV_A);
-	printf("\n");
-	printf("Vektor V_B:\n");
-	vprint(pvV_B);
-	printf("\n");
+
+	// Runge Kutta
 
 
 	//Gebe nicht mehr benötigten Arbeitspeicherfrei:
-	free_mat(pmA);
-	free_mat(pmB);
-	free_vec(pvV);
-	free_vec(pvV_A);
-	free_vec(pvV_B);
-
-
-
+	free_mat(pmA1);
+	free_mat(pmA2);
+	free_mat(pmA3);
+	free_vec(pvIC1);
+	free_vec(pvIC2);
+	free_vec(pvIC3);
 
 
 
